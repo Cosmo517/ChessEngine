b2f83a558cac081c12f66467d55abc74f8613435
Ethan Bielecki <ebielecki2003@gmail.com>
2024-05-11 02:32:04 -0400

    Working on creating cells

--- ChessUI/MainWindow.xaml.cs
+++ ChessUI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,14 +12,48 @@ using System.Windows.Shapes;
 
 namespace ChessUI
 {
+    class Cell
+    {
+        string color;
+        Image piece;
+        public Cell(String color, Image piece)
+        {
+            this.color = color;
+            this.piece = piece;
+        }
+
+        public string getColor() { return color; }
+
+        public void setColor(String color) { this.color = color; }
+
+        public Image getPiece() { return piece; }
+
+        public void setPiece(Image piece) {  this.piece = piece; }
+
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Cell[,] cells = new Cell[8, 8];
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeBackground();
+        }
+
+        private void InitializeBackground()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j =  0; j < 8; j++)
+                {
+
+                }
+            }
         }
     }
 }
